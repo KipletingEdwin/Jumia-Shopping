a1953cbace652e99c25b6dcdf136221c072eb185
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-07-04 16:47:42 +0300

    add two icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import styles from './Navbar.module.css'
+import deliveryImage from '../../assets/navbar/freedelivery.gif'
+import jumiaIcon from '../../assets/navbar/jumia.png'
 
 function Navbar() {
   return (
-    <div>
-        Navbar
-    </div>
+    <section className={styles.container} >
+
+      <div className={styles.delivery}>
+        <img src={deliveryImage} alt="delivery" className="delivery-image" />
+      </div>
+
+      <div className={styles.sell}>
+        <div className={styles.sellSection}>
+          <img src={jumiaIcon} alt="jumiaIcon" className="sellIcon" />
+
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
